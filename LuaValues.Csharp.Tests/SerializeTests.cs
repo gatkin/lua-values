18fe406279a47feaab7752d159758c1601ecd14a
gatkin <gregscottatkin@gmail.com>
2018-07-08 11:04:19 -0500

    Use Nunit for tests

--- LuaValues.Csharp.Tests/SerializeTests.cs
+++ LuaValues.Csharp.Tests/SerializeTests.cs
@@ -1,12 +1,12 @@
-using System;
-using Xunit;
+using NUnit.Framework;
 
-namespace LuaValues.Tests
+namespace LuaValues.CSharp.Tests
 {
+    [TestFixture]
     public class SerializeTests
     {
-        [Fact]
-        public void TestAggregate()
+        [Test]
+        public void Serialize_AnonymousClass()
         {
             var value = new {
                 message = "Hello",
@@ -17,13 +17,11 @@ namespace LuaValues.Tests
 
             var expected = @"{message=""Hello"", intNumber=1256, floatNumber=3.14, flag=true}";
 
-            var actual = LuaValues.ToLuaChunk(value);
-
-            Assert.Equal(expected, actual);
+            TestSerialize(value, expected);
         }
 
-        [Fact]
-        public void TestAggregateNested()
+        [Test]
+        public void Serialize_AnonymousClass_Nested()
         {
             var value = new {
                 message = "Hello",
@@ -38,25 +36,11 @@ namespace LuaValues.Tests
 
             var expected = @"{message=""Hello"", data={number=37, moreData={flag=false, values={37, 15, 72}}}}";
 
-            var actual = LuaValues.ToLuaChunk(value);
-
-            Assert.Equal(expected, actual);
+            TestSerialize(value, expected);
         }
 
-        [Fact]
-        public void TestArrayEmpty()
-        {
-            var value = new int[0];
-
-            var expected = "{}";
-
-            var actual = LuaValues.ToLuaChunk(value);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void TestArrayOfAggregates()
+        [Test]
+        public void Serialize_Array_AnonymouClass()
         {
             var value = new[] {
                 new { X = "Hello", Y = 37 },
@@ -65,60 +49,60 @@ namespace LuaValues.Tests
 
             var expected = @"{{X=""Hello"", Y=37}, {X=""Goodbye"", Y=42}}";
 
-            var actual = LuaValues.ToLuaChunk(value);
-
-            Assert.Equal(expected, actual);
+            TestSerialize(value, expected);
         }
 
-        [Fact]
-        public void TestArrayOfBooleans()
+        [Test]
+        public void Serailize_Array_Booleans()
         {
             var value = new [] { true, true, false };
             
             var expected = "{true, true, false}";
 
-            var actual = LuaValues.ToLuaChunk(value);
-
-            Assert.Equal(expected, actual);
+            TestSerialize(value, expected);
         }
 
-        [Fact]
-        public void TestArrayOfFloats()
+        [Test]
+        public void Serialize_Array_Empty()
+        {
+            var value = new int[0];
+
+            var expected = "{}";
+
+            TestSerialize(value, expected);
+        }
+
+        [Test]
+        public void Serialize_Array_Floats()
         {
             var value = new [] { 17.23, 42.1, 789.01 };
             
             var expected = "{17.23, 42.1, 789.01}";
 
-            var actual = LuaValues.ToLuaChunk(value);
-
-            Assert.Equal(expected, actual);
+            TestSerialize(value, expected);
         }
 
-        [Fact]
-        public void TestArrayOfIntegers()
+        [Test]
+        public void Serialize_Array_Integers()
         {
             var value = new [] { 17, 42, 789, 23 };
             
             var expected = "{17, 42, 789, 23}";
 
-            var actual = LuaValues.ToLuaChunk(value);
-
-            Assert.Equal(expected, actual);
+            TestSerialize(value, expected);
         }
 
-        [Fact]
-        public void TestArrayOfStrings()
+        [Test]
+        public void Serialize_Array_Strings()
         {
             var value = new [] { "hello", "world", "goodbye", "mars" };
             
             var expected = @"{""hello"", ""world"", ""goodbye"", ""mars""}";
 
-            var actual = LuaValues.ToLuaChunk(value);
-
-            Assert.Equal(expected, actual);
+            TestSerialize(value, expected);
         }
 
-        [Fact]
+        [Test]
         public void TestBoolean()
         {
             var testCases = new [] {
@@ -128,28 +112,25 @@ namespace LuaValues.Tests
 
             foreach (var (value, expected) in testCases)
             {
-                var actual = LuaValues.ToLuaChunk(value);
-                Assert.Equal(expected, actual);
+                TestSerialize(value, expected);
             }
         }
 
-        [Fact]
-        void TestNil()
+        [Test]
+        public void TestNil()
         {
-            var actual = LuaValues.ToLuaChunk(null);
-            Assert.Equal("nil", actual);
+            TestSerialize(null, "nil");
         }
 
-        [Fact]
-        void TestNilInAggregate()
+        [Test]
+        public void TestNilInAggregate()
         {
             var value = new { X = 1, Y = (string)null };
             var expected = "{X=1, Y=nil}";
-            var actual = LuaValues.ToLuaChunk(value);
-            Assert.Equal(expected, actual);
+            TestSerialize(value, expected);
         }
 
-        [Fact]
+        [Test]
         public void TestNumbers()
         {
             var testCases = new[] {
@@ -162,12 +143,11 @@ namespace LuaValues.Tests
 
             foreach (var (value, expected) in testCases)
             {
-                var actual = LuaValues.ToLuaChunk(value);
-                Assert.Equal(expected, actual);
+                TestSerialize(value, expected);
             }
         }
 
-        [Fact]
+        [Test]
         public void TestStrings()
         {
             var testCases = new[] {
@@ -177,12 +157,11 @@ namespace LuaValues.Tests
 
             foreach (var (value, expected) in testCases)
             {
-                var actual = LuaValues.ToLuaChunk(value);
-                Assert.Equal(expected, actual);
+                TestSerialize(value, expected);
             }
         }
 
-        [Fact]
+        [Test]
         public void TestReadmeExample()
         {
             var value = new {
@@ -198,9 +177,13 @@ namespace LuaValues.Tests
 
             var expected = @"{message=""Hello"", number=125, flag=true, measurements={1.23, 4.56, 7.89}, subValue={message=""Goodbye"", flag=false}}";
 
-            var actual = LuaValues.ToLuaChunk(value);
+            TestSerialize(value, expected);
+        }
 
-            Assert.Equal(expected, actual);
+        private void TestSerialize(object value, string expected)
+        {
+            var actual = LuaValues.ToLuaChunk(value);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- LuaValues.Tests/Parse.fs
+++ LuaValues.Tests/Parse.fs
@@ -1,88 +1,90 @@
-module Tests
+module LuaValues.Test
 
-open Xunit
+open NUnit.Framework
 open LuaValues.LuaTypes
 open LuaValues.Parse
 
+[<TestFixture>]
+type TestParse () =
 
-let private testParse input expected =
-    let actual = parseValue input
-    Assert.Equal(expected, actual)
+    let testParse input expected =
+        let actual = parseValue input
+        Assert.AreEqual(expected, actual)
 
-let private testSuccess input expected =
-    testParse input (Some expected)
+    let testSuccess input expected =
+        testParse input (Some expected)
 
-[<Fact>]
-let ``Parse nil`` () =
-    testParse "nil" (Some LuaNil)
+    [<Test>]
+    member this.``Parse nil`` () =
+        testParse "nil" (Some LuaNil)
 
-[<Fact>]
-let ``Parse nil with whitespace`` () =
-    testSuccess "  nil " LuaNil
+    [<Test>]
+    member this.``Parse nil with whitespace`` () =
+        testSuccess "  nil " LuaNil
 
-[<Fact>]
-let ``Parse false`` () =
-    testSuccess "false" (LuaBoolean false)
+    [<Test>]
+    member this.``Parse false`` () =
+        testSuccess "false" (LuaBoolean false)
 
-[<Fact>]
-let ``Parse true`` () =
-    testSuccess "true" (LuaBoolean true)
+    [<Test>]
+    member this.``Parse true`` () =
+        testSuccess "true" (LuaBoolean true)
 
-[<Fact>]
-let ``Parse number`` () =
-    testSuccess "3.14" (LuaNumber 3.14)
+    [<Test>]
+    member this.``Parse number`` () =
+        testSuccess "3.14" (LuaNumber 3.14)
 
-[<Fact>]
-let ``Parse single quoted string`` () =
-    testSuccess "'Hello, World!'" (LuaString "Hello, World!")
+    [<Test>]
+    member this.``Parse single quoted string`` () =
+        testSuccess "'Hello, World!'" (LuaString "Hello, World!")
 
-[<Fact>]
-let ``Parse double quoted string`` () =
-    testSuccess "\"Go Big Red!\"" (LuaString "Go Big Red!")
+    [<Test>]
+    member this.``Parse double quoted string`` () =
+        testSuccess "\"Go Big Red!\"" (LuaString "Go Big Red!")
 
-[<Fact>]
-let ``Parse single quoted string with double quotes inside`` () =
-    testSuccess "'He said \"Hello\" to her'" (LuaString "He said \"Hello\" to her" )
+    [<Test>]
+    member this.``Parse single quoted string with double quotes inside`` () =
+        testSuccess "'He said \"Hello\" to her'" (LuaString "He said \"Hello\" to her" )
 
-[<Fact>]
-let ``Parse double quoted string with single quotes inside`` () =
-    testSuccess "\"She said 'Goodbye' to him\"" (LuaString "She said 'Goodbye' to him")
+    [<Test>]
+    member this.``Parse double quoted string with single quotes inside`` () =
+        testSuccess "\"She said 'Goodbye' to him\"" (LuaString "She said 'Goodbye' to him")
 
-[<Fact>]
-let ``Parse array of numbers`` () =
-    testSuccess "{1,2,3}" (LuaArray [LuaNumber 1.0; LuaNumber 2.0; LuaNumber 3.0])
+    [<Test>]
+    member this.``Parse array of numbers`` () =
+        testSuccess "{1,2,3}" (LuaArray [LuaNumber 1.0; LuaNumber 2.0; LuaNumber 3.0])
 
-[<Fact>]
-let ``Parse array of numbers with whitespace`` () =
-    testSuccess "{ 1 ,  2,3 ; 4  }" (LuaArray [LuaNumber 1.0; LuaNumber 2.0; LuaNumber 3.0; LuaNumber 4.0])
+    [<Test>]
+    member this.``Parse array of numbers with whitespace`` () =
+        testSuccess "{ 1 ,  2,3 ; 4  }" (LuaArray [LuaNumber 1.0; LuaNumber 2.0; LuaNumber 3.0; LuaNumber 4.0])
 
-[<Fact>]
-let ``Parse nested array`` () =
-    let expected =
-        LuaArray [LuaNumber 1.0
-                  LuaArray [LuaString "hello"
-                            LuaString "bye"
-                            LuaArray [ LuaBoolean false ] 
-                            LuaString "a"]
-                  LuaNumber 2.0
-                  LuaArray [LuaBoolean true; LuaNil]]
+    [<Test>]
+    member this.``Parse nested array`` () =
+        let expected =
+            LuaArray [LuaNumber 1.0
+                      LuaArray [LuaString "hello"
+                                LuaString "bye"
+                                LuaArray [ LuaBoolean false ] 
+                                LuaString "a"]
+                      LuaNumber 2.0
+                      LuaArray [LuaBoolean true; LuaNil]]
 
-    testSuccess "{1, {'hello', 'bye', {false}, 'a'}, 2, {true, nil}}" expected
+        testSuccess "{1, {'hello', 'bye', {false}, 'a'}, 2, {true, nil}}" expected
 
-[<Fact>]
-let ``Parse table`` () =
-    testSuccess "{x = 1, y = 2}" (LuaTable [ { Name = "x"; Value = LuaNumber 1.0 }; { Name = "y"; Value = LuaNumber 2.0 } ])
+    [<Test>]
+    member this.``Parse table`` () =
+        testSuccess "{x = 1, y = 2}" (LuaTable [ { Name = "x"; Value = LuaNumber 1.0 }; { Name = "y"; Value = LuaNumber 2.0 } ])
 
-[<Fact>]
-let ``Parse empty table`` () =
-    testSuccess "{}" (LuaTable [])
+    [<Test>]
+    member this.``Parse empty table`` () =
+        testSuccess "{}" (LuaTable [])
 
-[<Fact>]
-let ``Parse nested table`` () =
-    let expected =
-        LuaTable [{ Name = "x"; Value = LuaNumber 1.0 }
-                  { Name = "y"; Value = LuaTable [{ Name = "a"; Value = LuaBoolean true }
-                                                  { Name = "b"; Value = LuaArray [LuaNumber 1.0; LuaNumber 2.0] }]}
-                  { Name = "x"; Value = LuaNil }]
+    [<Test>]
+    member this.``Parse nested table`` () =
+        let expected =
+            LuaTable [{ Name = "x"; Value = LuaNumber 1.0 }
+                      { Name = "y"; Value = LuaTable [{ Name = "a"; Value = LuaBoolean true }
+                                                      { Name = "b"; Value = LuaArray [LuaNumber 1.0; LuaNumber 2.0] }]}
+                      { Name = "x"; Value = LuaNil }]
 
-    testSuccess "{x = 1, y = {a = true, b = {1, 2}}, x = nil}" expected
+        testSuccess "{x = 1, y = {a = true, b = {1, 2}}, x = nil}" expected
